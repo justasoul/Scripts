659642b865b7465aa59ce7d15ae1ad51f9fe6dc3
justasoul <justasoul@hotmail.com>
2019-08-13 13:39:31 +0100

    Create call_sqlplus.bat

---
+++ Batch/call_sqlplus.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set oracle_connect_string=%1
+set sql_file_to_run=%2
+
+chcp 1252
+
+set "callsqlplus=exit | sqlplus %oracle_connect_string% @"
+
+set runme=%callsqlplus%%sql_file_to_run%
+
+REM %runme%
